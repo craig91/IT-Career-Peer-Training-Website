c6b6b3cc02221277d7d00cea2369fb28d080a5fe
craig91 <craigdejean1@gmail.com>
2024-10-14 15:16:32 -0400

    removed test console logs

--- scripts.js
+++ scripts.js
@@ -4,8 +4,3 @@ const navLinks = document.querySelector('.nav-links');
 buger.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
-
-console.log("I am working with discord")
-console.log("Another test");
-console.log("and another test")
-console.log("test again again")
